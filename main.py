1bfc08bd2ac0165e0fa9717bb00d2733345831c3
ziyadsankoh <ziyadsankoh14@gmail.com>
2024-11-13 19:48:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,19 +23,10 @@ for line in waste_read.split("\n"):
         column_names = line.split(",")
         column_names_read = True
     else:
-<<<<<<< Updated upstream
-        L = line.split(",")
-        if len(L)>1:
-            waste_data[L[2]] = {"gdp_per_capita":L[4], "composition_food_organic_waste_percent":L[5],"composition_glass_percent":L[6],
-                "composition_metal_percent":L[7],"composition_other_percent":L[8],"composition_paper_cardboard_percent":L[9],
-                "composition_plastic_percent":L[10],"population_population_number_of_people":L[11],"special_waste_e_waste_tons_year":L[12],
-                "total_msw_generated_tons_year":L[13]}
-=======
         waste_data.append(line.split(","))
 
 median_age_read = "median.age.csw".split()
 start = dt.date.today() - dt.timedelta(365)
->>>>>>> Stashed changes
 
 for line in median_age_read.split("\n"):
     if not column_names_read:
